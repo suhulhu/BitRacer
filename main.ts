3093c1e808a5ddeba40118316a40d2c8fb389922
suhulhu <67456710+suhulhu@users.noreply.github.com>
2021-10-13 17:07:56 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,8 @@
+//% color="#AA278D" weight=100
+namespace BirRacer {
+    //% block
+    export function irRead(address: number): number {
+        pins.i2cWriteNumber(16,  4,  NumberFormat.Int8LE,  false)
+        return pins.i2cReadNumber(16, NumberFormat.UInt16BE, false)
+    }
+}
