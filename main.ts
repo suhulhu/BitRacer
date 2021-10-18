11b0537bbb3cae1c65c5e62b8354c255a93d0572
suhulhu <67456710+suhulhu@users.noreply.github.com>
2021-10-18 11:45:17 +0800

    更新 main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 //% color="#AA278D" weight=100
 namespace BirRacer {
     //% block
+    //% blockId=sensor_readIR block="read IRsensor |%address"
     export function irRead(address: number): number {
         pins.i2cWriteNumber(16,  4,  NumberFormat.Int8LE,  false)
         return pins.i2cReadNumber(16, NumberFormat.UInt16BE, false)
