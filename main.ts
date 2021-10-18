4c356eaef9ee01cfc08537188cdda0d6f7076ced
suhulhu <67456710+suhulhu@users.noreply.github.com>
2021-10-18 11:56:52 +0800

    更新 main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,11 @@
+const N76_ADDR = 0x10
 //% color="#AA278D" weight=100
 namespace BirRacer {
     //% block
     //% blockId=sensor_readIR block="read IRsensor |%address"
+    //% SensorIDs.min=1 SensorIDs.max=5
     export function irRead(address: number): number {
-        pins.i2cWriteNumber(16,  4,  NumberFormat.Int8LE,  false)
+        pins.i2cWriteNumber(N76_ADDR,  address+2,  NumberFormat.UInt8LE,  false)
         return pins.i2cReadNumber(16, NumberFormat.UInt16BE, false)
     }
 
