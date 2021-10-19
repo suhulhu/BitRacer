ffed0573e2eb19cec892b17f4121a7c19c416059
suhulhu <67456710+suhulhu@users.noreply.github.com>
2021-10-19 08:28:24 +0800

    更新 main.ts

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@ const N76_ADDR = 0x10
 namespace BitRacer {
     //% block
     //% blockId=sensor_readIR block="read IRsensor |%address"
-    //% SensorIDs.min=1 SensorIDs.max=5
+    //% address.min=1 address.max=5 address.defl=1
     export function irRead(address: number): number {
         pins.i2cWriteNumber(N76_ADDR,  address+2,  NumberFormat.UInt8LE,  false)
         return pins.i2cReadNumber(16, NumberFormat.UInt16BE, false)
