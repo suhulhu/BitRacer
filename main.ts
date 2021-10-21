285de502f1b24129d07dd9b8e5125b5b2a22b385
suhulhu <67456710+suhulhu@users.noreply.github.com>
2021-10-21 14:09:02 +0800

    更新 main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 const N76_ADDR = 0x10
-//% color="#AA278D" weight=50
+//% color="#AA278D" icon="\uf1ba" weight=50
 namespace BitRacer {
     //% block
     //% blockId=sensor_readIR block="read IRsensor |%address"
@@ -7,6 +7,23 @@ namespace BitRacer {
     export function irRead(address: number): number {
         pins.i2cWriteNumber(N76_ADDR,  address+2,  NumberFormat.UInt8LE,  false)
         return pins.i2cReadNumber(N76_ADDR, NumberFormat.UInt16BE, false)
+    }
+    //% blockId=motor_motorRun block="motor|%index|PWM value|%PWM"
+    //% PWM.min=-1000 PWM.max=1000
+    export function motorRun(index: number, PWM: number): void {
+        let i2cbuf = pins.createBuffer(3);
+        if (index == 0) {
+            i2cbuf[0] = 0x00;
+            i2cbuf[1] = (PWM >> 8);
+            i2cbuf[2] = PWM;
+            pins.i2cWriteBuffer(N76_ADDR, i2cbuf);
+        }
+        if (index == 2) {
+            i2cbuf[0] = 0x02;
+            i2cbuf[1] = (PWM >> 8);
+            i2cbuf[2] = PWM;
+            pins.i2cWriteBuffer(N76_ADDR, i2cbuf);
+        }
     }
 
 }
